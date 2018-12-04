58a9d4ca61058e48f1a8e7dfe96211000abdfd09
Lucas Hipólito <lukhipolito@yahoo.com.br>
2018-12-04 12:20:06 -0200

    Solution and files

---
+++ LucasMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LucasMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LucasMVC/Models/GreetingsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LucasMVC.Models
+{
+    public class GreetingsModel
+    {
+        public string Name { get; set; }
+        public string Greeting { get; set; }
+    }
+}
